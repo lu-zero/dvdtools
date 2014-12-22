1c9d492163dba44c0d00352d4a936615429995ac
Luca Barbato <lu_zero@gentoo.org>
2014-12-22 15:32:18 +0100

    wip:

--- test_dvd.sh
+++ test_dvd.sh
@@ -1,12 +1,14 @@
-for a in /mnt/vbox/nouveautés/*; do
-    mount -o loop $a /mnt/loop
-    for b in /mnt/loop/VIDEO_TS/VIDEO.TS /mnt/loop/VIDEO_TS/VTS_*_0.VOB; do
+time for a in /mnt/vbox/*/*; do
+    mkdir /mnt/work/t -p
+    echo "Processing $a"
+    iso2vob -i $a -o /mnt/work/t/
+    empty=No
+    for b in /mnt/work/t/VIDEO_TS/VIDEO.TS /mnt/work/t/VIDEO_TS/VTS_*_0.VOB; do
         if ./print_vobu $b 2>&1 | grep Empty; then
-            echo $a $b >> /tmp/empty-nav.log
-            echo $a $b Empty
-        else
-            echo $a $b Ok
+            empty=Yes
         fi
     done
-    umount /mnt/loop
+
+    echo $a >> /tmp/em-${empty}.log
+    rm -fR /mnt/work/t/*
 done
